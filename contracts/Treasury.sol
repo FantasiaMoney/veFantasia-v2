581706acba361b383405ed4c475b1b2623dd2101
RuslanMirov <ruslan777mirov@gmail.com>
2022-02-14 13:13:32 +0200

    Update

--- contracts/Treasury.sol
+++ contracts/Treasury.sol
@@ -7,7 +7,13 @@ import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 
 contract Treasury is Ownable {
   using SafeERC20 for IERC20;
-  
+
+  // deposit in strategy
+
+  // withdraw from strategy
+
+  // set strategy
+
   function withdraw(uint256 _amount, address _token) external onlyOwner {
     IERC20(_token).safeTransfer(msg.sender, _amount);
   }
